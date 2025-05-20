9780d5147de94e0f75f8692663d156b5865a7448
Bhaskar JS <bhaskarsqaedevops@gmail.com>
2025-05-20 05:12:26 +0000

    added data for Location dropdown menu and Request a callback menu and create images folder and output.css folder

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                strong-blue: '#1e3a8a',
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
